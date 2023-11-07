285a1d28dbb782822f2a8635f59b0b2730f3098c
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-11-07 19:36:07 +0300

    root page architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
 import {Routes,Route} from "react-router-dom";
+import "./globals.css";
+import SignupForm from "./_auth/forms/SignupForm";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
           {/* public Routes*/}
-
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
           {/* private Routes*/}
+          <Route index element={<Home />} />
       </Routes>
     </main>
   )
