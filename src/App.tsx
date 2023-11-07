26bd5805361c1a21bb8a9ff2ea99b642158b36cc
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-11-07 19:43:15 +0300

    use layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import { Home } from "./_root/pages";
 import {Routes,Route} from "react-router-dom";
 import "./globals.css";
 import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
           {/* public Routes*/}
-          <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<SignupForm />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-in" element={<SigninForm />} />
+            <Route path="/sign-up" element={<SignupForm />} />
+          </Route>
           {/* private Routes*/}
-          <Route index element={<Home />} />
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+          </Route>
       </Routes>
     </main>
   )
