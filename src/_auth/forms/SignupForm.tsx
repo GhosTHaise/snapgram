52bbd5bb95e6df7cc881d8189aef3d43f156448a
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-11-09 20:51:26 +0300

    zod schema

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,39 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import {useForm} from "react-hook-form"
 import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { signupValidation } from "@/lib/validation"
+
+
+
 const SignupForm = () => {
+  
+  const form = useForm<z.infer<typeof signupValidation>>({
+    resolver: zodResolver(signupValidation),
+    defaultValues: {
+      name : "",
+      username: "",
+      email : "",
+      password : ""
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof signupValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
   return (
     <div>
       <Button>
