a7405fc6f241f9bd5423726e18bf236d6b888775
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-11-21 20:21:31 +0300

    backend : signout function

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,8 +1,19 @@
-import { Link } from "lucide-react"
+import { Link , useNavigate } from "react-router-dom"
 import { Button } from "../ui/button"
+import { userSignOutAccount } from "@/lib/react-query/queriesAndMutation"
+import { useEffect } from "react";
+import { useUserContext } from "@/context/AuthContext";
 
 
 const Topbar = () => {
+  const navigate = useNavigate();
+  const { mutate : signOut , isSuccess} = userSignOutAccount();
+  const {user} = useUserContext()
+  console.log(user);
+  
+  useEffect(() => {
+    if(isSuccess) navigate(0)
+  },[isSuccess])
   return (
     <section className="topbar">
         <div className="flex-between py-4 px-5">
@@ -15,9 +26,21 @@ const Topbar = () => {
             </Link>
 
             <div className="flex gap-4">
-                <Button variant={"ghost"} className="shad-button_ghost">
+                <Button 
+                  variant={"ghost"} 
+                  className="shad-button_ghost"
+                  onClick={() => signOut()}
+                >
                     <img src="/assets/icons/logout.svg" alt="logout"  />
                 </Button>
+                <Link 
+                    to={`/profile/${user.id}`} 
+                    className="flex-center gap-3" >
+                      <img 
+                        src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+                        alt="profile"
+                      />
+                </Link>
             </div>
         </div>
     </section>
--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -1,5 +1,5 @@
 import {useQuery , useMutation , useQueryClient, useInfiniteQuery} from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
 import { INewUser } from "@/types";
 
 export const userCreateUserAccount = () => {
@@ -11,6 +11,13 @@ export const userCreateUserAccount = () => {
 export const userSignInAccount = () => {
     return useMutation({
         mutationFn : (user : { email : string , password : string}) => signInAccount(user)
+    })
+}
+
+
+export const userSignOutAccount = () => {
+    return useMutation({
+        mutationFn : () => signOutAccount()
     })
 }
 
