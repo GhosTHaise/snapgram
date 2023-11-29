7ba19442d9998b3196a49d9b950410fb78f581a5
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-11-29 19:45:09 +0300

    frontend : install react-dropzone

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React , {useCallback, useState} from 'react'
+import { useDropzone } from 'react-dropzone'
 
 const FileUploader = () => {
+    const [fileUrl, setFileUrl] = useState<string | null>("")
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
-    <div>FileUploader</div>
+    <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'>
+      <input {...getInputProps()} className='cursor-pointer' />
+      {
+        fileUrl ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
   )
 }
 
