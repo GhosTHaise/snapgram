2b5f83bade2dd4897586eb8ee7ff11c1c6760140
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-12-01 19:28:24 +0300

    backend : upload file

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
-import { INewUser } from "@/types";
+import { INewPost, INewUser } from "@/types";
 import  { ID, Query } from "appwrite"
-import { account, appwriteconfig, avatars, databases } from "./config";
+import { account, appwriteconfig, avatars, databases, storage } from "./config";
 
 export async function createUserAccount(user:INewUser) {
     try {
@@ -82,6 +82,54 @@ export async function signOutAccount(){
     try {
         const session = await account.deleteSession("current");
         return session
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function createPost(post : INewPost){
+    try {
+        const uploadedFile = await uploadFile(post.file[0])
+
+        if(!uploadedFile) throw Error
+
+        //Get file url
+        const fileUrl = getFilePreview(uploadedFile.$id)
+
+        if(!fileUrl) {
+            throw Error
+        }
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+export async function uploadFile(file : File){
+    try {
+        const uploadedFile = await storage.createFile(
+            appwriteconfig.storageId,
+            ID.unique(),
+            file
+        )
+
+        return uploadedFile;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getFilePreview(fileId : string){
+    try {
+        const fileUrl = storage.getFilePreview(
+            appwriteconfig.storageId,
+            fileId,
+            2000,
+            2000,
+            "top",
+            100
+        )
+
+        return fileUrl
     } catch (error) {
         console.log(error);
     }
