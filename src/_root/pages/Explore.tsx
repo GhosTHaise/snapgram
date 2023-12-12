4a106da458c1495690ce0e8130f87adf067c8e54
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-12-12 20:09:22 +0300

    frontend : filter button

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -24,6 +24,24 @@ const Explore = () => {
                     />
             </div>
         </div>  
+
+        <div className='flex-between w-full max-w-5xl mt-16 mb-7'>
+            <h2 className='body-bold md:h3-bold w-full'>Popular Today</h2>
+
+            <div className='flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer'>
+                <p className='small-medium md:base-medium text-light-2'>All</p>
+                <img 
+                  src="/assets/icons/filter.svg" 
+                  width={20}
+                  height={20}
+                  alt="filter" 
+                  className="" />
+            </div>
+        </div>    
+
+        <div className='flex flex-wrap gap-9 w-full max-w-5xl'>
+            
+        </div>    
     </div>
   )
 }
