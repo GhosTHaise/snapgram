f0881188602f1be7e3a3bdf06ae5b95e20821dec
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-12-16 08:50:47 +0300

    frontedn : infinite scroll

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -5,17 +5,21 @@ import { Input } from '@/components/ui/input'
 import useDebounce from '@/hooks/useDebounce'
 import { useGetPosts, useSearchPosts } from '@/lib/react-query/queriesAndMutation'
 import { Models } from 'appwrite'
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 const Explore = () => {
-
-  const { data : posts , fetchNexPage, hasNextPage } = useGetPosts();
+  const { ref , inView } = useInView()
+  const { data : posts , fetchNextPage, hasNextPage } = useGetPosts();
 
   const [searchValue, setSearchValue] = useState<string>("")
   
   const debouncedValue = useDebounce(searchValue, 500);
   const { data : searchedPosts , isFetching : isSearchFetching } = useSearchPosts(debouncedValue)
+
+  useEffect(()=>{
+    if(inView && !searchValue) fetchNextPage()
+  },[inView,searchValue])
 
   if(!posts) {
     return(
@@ -83,7 +87,15 @@ const Explore = () => {
                     />
                 ))
                 }
-        </div>    
+        </div>
+
+        {
+          hasNextPage && !searchValue && (
+            <div ref={ref} className='mt-10'>
+                <Loader />
+            </div>  
+          )
+        }
     </div>
   )
 }
